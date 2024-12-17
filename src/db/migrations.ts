793c1803609cebe13b3c31b9bf4261f459cb7328
Ray <raypeaslee@gmail.com>
2024-12-17 14:31:57 -0500

    add auth tables to database

--- src/db/migrations.ts
+++ src/db/migrations.ts
@@ -21,9 +21,21 @@ migrations['001'] = {
       .addColumn('service', 'varchar', (col) => col.primaryKey())
       .addColumn('cursor', 'integer', (col) => col.notNull())
       .execute()
+    await db.schema
+      .createTable('auth_session')
+      .addColumn('key', 'varchar', (col) => col.primaryKey())
+      .addColumn('session', 'varchar', (col) => col.notNull())
+      .execute()
+    await db.schema
+      .createTable('auth_state')
+      .addColumn('key', 'varchar', (col) => col.primaryKey())
+      .addColumn('state', 'varchar', (col) => col.notNull())
+      .execute()
   },
   async down(db: Kysely<unknown>) {
     await db.schema.dropTable('post').execute()
     await db.schema.dropTable('sub_state').execute()
+    await db.schema.dropTable('auth_state').execute()
+    await db.schema.dropTable('auth_session').execute()
   },
 }
