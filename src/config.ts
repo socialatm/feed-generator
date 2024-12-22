be357f31eb752121ca16250544fd18f25db1cf98
Ray <raypeaslee@gmail.com>
2024-12-22 08:08:00 -0500

    Update config.ts
    add 'resolver' to 'AppContext'

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ import { Database } from './db'
 import { DidResolver } from '@atproto/identity'
 import type { OAuthClient } from '@atproto/oauth-client-node'
 import { pino } from 'pino'
+import { createBidirectionalResolver, createIdResolver, BidirectionalResolver } from './id-resolver'
 
 export type AppContext = {
   db: Database
@@ -9,6 +10,7 @@ export type AppContext = {
   cfg: Config
   oauthClient: OAuthClient
   logger: pino.Logger
+  resolver: BidirectionalResolver
 }
 
 export type Config = {
